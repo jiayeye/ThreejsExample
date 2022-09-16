4ea06e7c6dc8bed9745a2d3a52bebeba190cf63f
jiayeye <yecheng.zj@gmail.com>
2022-09-16 09:03:16 +0800

    feat: fix name

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,26 @@
 <script setup>
-  import ThreeTest from './components/ShowModel.vue'
+  import ShowModel from './components/ShowModel.vue'
 </script>
   
   <template>
     <!-- 电视 -->
-    <ThreeTest modelUrl= "http://rhkbqy9os.hn-bkt.clouddn.com/dianshi.fbx?e=1663222785&token=OkXHJZMiNUEjV5wqnHmy73EoxxBwOWPqA6co-0Qv:xhpUwZIfl6kv_fE6_1FieVfIsM0="/>
+    <ShowModel modelUrl= "http://rhkbqy9os.hn-bkt.clouddn.com/dianshi.fbx?e=1663222785&token=OkXHJZMiNUEjV5wqnHmy73EoxxBwOWPqA6co-0Qv:xhpUwZIfl6kv_fE6_1FieVfIsM0="/>
   
     <!-- 洗衣机-->
-    <!-- <ThreeTest modelUrl= "http://rhkbqy9os.hn-bkt.clouddn.com/%E6%B5%B7%E5%B0%94-%E6%BB%9A%E7%AD%92-%E5%B9%B2%E8%A1%A3%E6%9C%BA-10kg-HGS100-306.fbx?e=1663237817&token=OkXHJZMiNUEjV5wqnHmy73EoxxBwOWPqA6co-0Qv:01KaJPjlnh6ndbEoII6AwpfAPjM="/> -->
+    <!-- <ShowModel modelUrl= "http://rhkbqy9os.hn-bkt.clouddn.com/%E6%B5%B7%E5%B0%94-%E6%BB%9A%E7%AD%92-%E5%B9%B2%E8%A1%A3%E6%9C%BA-10kg-HGS100-306.fbx?e=1663237817&token=OkXHJZMiNUEjV5wqnHmy73EoxxBwOWPqA6co-0Qv:01KaJPjlnh6ndbEoII6AwpfAPjM="/> -->
   
     <!-- 风扇 -->
-    <!-- <ThreeTest modelUrl= "http://rhkbqy9os.hn-bkt.clouddn.com/fengshan.fbx?e=1663232853&token=OkXHJZMiNUEjV5wqnHmy73EoxxBwOWPqA6co-0Qv:FKpmgfjP-CiChIPwHCkYocl_Xtk="/> -->
+    <!-- <ShowModel modelUrl= "http://rhkbqy9os.hn-bkt.clouddn.com/fengshan.fbx?e=1663232853&token=OkXHJZMiNUEjV5wqnHmy73EoxxBwOWPqA6co-0Qv:FKpmgfjP-CiChIPwHCkYocl_Xtk="/> -->
   
     <!-- 冰箱 -->
-    <!-- <ThreeTest modelUrl= "http://rhkbqy9os.hn-bkt.clouddn.com/BC11K3000.fbx?e=1663236284&token=OkXHJZMiNUEjV5wqnHmy73EoxxBwOWPqA6co-0Qv:Maw_XhkDCXzUnENjFsvhPFr84tQ="/> -->
+    <!-- <ShowModel modelUrl= "http://rhkbqy9os.hn-bkt.clouddn.com/BC11K3000.fbx?e=1663236284&token=OkXHJZMiNUEjV5wqnHmy73EoxxBwOWPqA6co-0Qv:Maw_XhkDCXzUnENjFsvhPFr84tQ="/> -->
   
-    <!-- <ThreeTest modelUrl= "https://mz-reszgb.oss-cn-beijing.aliyuncs.com/lisongming_test_1/FY0018M0L-%E6%B5%B7%E5%B0%94OBT600-10SDA%E5%8E%A8%E7%94%B5.fbx"/> -->
-    <!-- <ThreeTest modelUrl= "https://mz-reszgb.oss-cn-beijing.aliyuncs.com/lisongming_test_1/FY0021M0L-%E6%B5%B7%E5%B0%94-ST450-30S%E5%8E%A8%E7%94%B5.fbx" /> -->
-    <!-- <ThreeTest modelUrl= "https://mz-reszgb.oss-cn-beijing.aliyuncs.com/lisongming_test_1/XNTJZHJT00227-%E5%8D%A1%E8%90%A8%E5%B8%9Dc7-%E6%B2%B9%E7%83%9F%E6%9C%BACXW-219-C7T90BGU1.fbx"/> -->
-    <!-- <ThreeTest modelUrl= "https://mz-reszgb.oss-cn-beijing.aliyuncs.com/lisongming_test/AB92G1012-海尔-KFR-50LW_01ABA81AU1套机-家空.fbx"/> -->
-    <!-- <ThreeTest modelUrl= "https://mz-reszgb.oss-cn-beijing.aliyuncs.com/lisongming_test/GK0GX800D-3D模型-空气能热泵-热水器-200L (2).fbx"/> -->
-    <!-- <ThreeTest modelUrl= "https://mz-reszgb.oss-cn-beijing.aliyuncs.com/lisongming_test/GK0GX300C-空气能热泵-热水器-200L-.fbx"/> -->
+    <!-- <ShowModel modelUrl= "https://mz-reszgb.oss-cn-beijing.aliyuncs.com/lisongming_test_1/FY0018M0L-%E6%B5%B7%E5%B0%94OBT600-10SDA%E5%8E%A8%E7%94%B5.fbx"/> -->
+    <!-- <ShowModel modelUrl= "https://mz-reszgb.oss-cn-beijing.aliyuncs.com/lisongming_test_1/FY0021M0L-%E6%B5%B7%E5%B0%94-ST450-30S%E5%8E%A8%E7%94%B5.fbx" /> -->
+    <!-- <ShowModel modelUrl= "https://mz-reszgb.oss-cn-beijing.aliyuncs.com/lisongming_test_1/XNTJZHJT00227-%E5%8D%A1%E8%90%A8%E5%B8%9Dc7-%E6%B2%B9%E7%83%9F%E6%9C%BACXW-219-C7T90BGU1.fbx"/> -->
+    <!-- <ShowModel modelUrl= "https://mz-reszgb.oss-cn-beijing.aliyuncs.com/lisongming_test/AB92G1012-海尔-KFR-50LW_01ABA81AU1套机-家空.fbx"/> -->
+    <!-- <ShowModel modelUrl= "https://mz-reszgb.oss-cn-beijing.aliyuncs.com/lisongming_test/GK0GX800D-3D模型-空气能热泵-热水器-200L (2).fbx"/> -->
+    <!-- <ShowModel modelUrl= "https://mz-reszgb.oss-cn-beijing.aliyuncs.com/lisongming_test/GK0GX300C-空气能热泵-热水器-200L-.fbx"/> -->
   </template>
   
 
