e411bd13865b534dc688bec6b9ce4f188ac7dd34
jiayeye <yecheng.zj@gmail.com>
2022-09-19 18:38:37 +0800

    feat: add measure label and lines

--- src/components/ShowModel.vue
+++ src/components/ShowModel.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div id="div3d">
     <div id="textDiv">
       <text id="errorText" v-if="showErrorInfo">加载失败，请检查资源</text>
     </div>
@@ -163,38 +163,70 @@ export default {
           // 显示canvas
           this.$refs.threeCanvas.hidden = false;
 
-          // 创建div
-          const moonMassDiv = document.createElement("div");
-          moonMassDiv.id = "css2d";
-          moonMassDiv.textContent = "7.342mm";
-          moonMassDiv.style.fontWeight = "600";
-          moonMassDiv.style.fontSize = "16px";
-          moonMassDiv.style.padding = "2px 10px";
-          moonMassDiv.style.backgroundColor = "white";
-          moonMassDiv.style.opacity = "0.7";
-          moonMassDiv.style.borderRadius = "15px";
-          const moonMassLabel = new CSS2DObject(moonMassDiv);
-          moonMassLabel.position.set(0, 0, 0);
-          object.add(moonMassLabel);
-
-          //create a blue LineBasicMaterial
-          const material = new THREE.LineBasicMaterial({ color: 0x0000ff });
+          // 包围位置为世界坐标，每次使用需要重新计算
+          const bbox1 = new THREE.Box3().setFromObject(object);
           const rightDownPoint = new THREE.Vector3(
-            bbox.max.x,
-            bbox.max.y,
-            bbox.max.z
+            bbox1.max.x,
+            bbox1.min.y,
+            bbox1.max.z
           );
           const leftDownPoint = new THREE.Vector3(
-            -bbox.max.x,
-            bbox.max.y,
-            bbox.max.z
+            -bbox1.max.x,
+            bbox1.min.y,
+            bbox1.max.z
+          );
+          // 创建线的材质
+          const material = new THREE.LineBasicMaterial({ color: 0x000000 });
+          // 设置线的关键点
+          const points = [rightDownPoint, leftDownPoint];
+          // 生成线
+          this.generateLine(points, material);
+          // 生成label
+          this.generateLabel(
+            object,
+            new THREE.Vector3(
+              (bbox.max.x + bbox.min.x) / 2,
+              bbox.min.y,
+              bbox.max.z
+            ),
+            bbox.max.x - bbox.min.x
           );
 
-          const points = [rightDownPoint, leftDownPoint];
-          const geometry = new THREE.BufferGeometry().setFromPoints(points);
+          const rightUpPoint = new THREE.Vector3(
+            bbox1.max.x,
+            bbox1.max.y,
+            bbox1.max.z
+          );
+          const points1 = [rightDownPoint, rightUpPoint];
+          this.generateLine(points1, material);
+          // 生成label
+          this.generateLabel(
+            object,
+            new THREE.Vector3(
+              bbox.max.x,
+              (bbox.max.y + bbox.min.y) / 2,
+              bbox.max.z
+            ),
+            bbox.max.y - bbox.min.y
+          );
 
-          const line = new THREE.Line(geometry, material);
-          scene.add(line);
+          const rightBackPoint = new THREE.Vector3(
+            bbox1.max.x,
+            bbox1.min.y,
+            bbox1.min.z
+          );
+          const points2 = [rightDownPoint, rightBackPoint];
+          this.generateLine(points2, material);
+          // 生成label
+          this.generateLabel(
+            object,
+            new THREE.Vector3(
+              bbox.max.x,
+              bbox.min.y,
+              (bbox.max.z + bbox.min.z ) / 2
+            ),
+            bbox.max.z - bbox.min.z
+          );
         },
         this.onProgress,
         this.onError
@@ -206,6 +238,36 @@ export default {
       window.addEventListener("mousedown", this.onMouseDown);
       // 设置tick
       this.animate();
+    },
+
+    // 生成线
+    generateLine(points, material) {
+      // 生成线的geometry
+      const geometry = new THREE.BufferGeometry().setFromPoints(points);
+      // 生成line mesh
+      const line = new THREE.Line(geometry, material);
+      // 添加到场景
+      scene.add(line);
+    },
+
+    // 生成2d标签
+    generateLabel(parentObject, position, distance) {
+      // 创建div
+      const divLabel = document.createElement("div");
+      // 设置标签样式
+      divLabel.textContent = Math.floor(distance).toString() + "mm";
+      divLabel.style.fontWeight = "600";
+      divLabel.style.fontSize = "16px";
+      divLabel.style.padding = "2px 10px";
+      divLabel.style.backgroundColor = "white";
+      divLabel.style.opacity = "0.85";
+      divLabel.style.borderRadius = "15px";
+      // 创建标签label
+      const label = new CSS2DObject(divLabel);
+      // 设置label在object中位置
+      label.position.set(position.x, position.y, position.z);
+      // 添加到object
+      parentObject.add(label);
     },
 
     // 加载进度
@@ -285,5 +347,10 @@ export default {
   line-height: 40px;
   text-align: center;
   top: 46%;
+}
+
+#div3d {
+  width: 100vh;
+  height: 100vw;
 }
 </style>
