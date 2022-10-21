ad3f68757838c5b6e04c1f3aafe3b1762d1a335a
周佳 <zhoujia.uh@haier.com>
2022-10-21 17:37:41 +0800

    feat: add change

--- src/App.vue
+++ src/App.vue
@@ -4,22 +4,17 @@ import ShowModel from "./components/ShowModel.vue";
 
 <template>
   <!-- 电视 -->
-  <ShowModel modelUrl= "http://rhkbqy9os.hn-bkt.clouddn.com/dianshi.fbx?e=1663222785&token=OkXHJZMiNUEjV5wqnHmy73EoxxBwOWPqA6co-0Qv:xhpUwZIfl6kv_fE6_1FieVfIsM0="/>
+  <ShowModel modelUrl= "https://syn-yf-design-tool.oss-cn-beijing.aliyuncs.com/save_fbx/60000AM64-模型.fbx"/>
 
   <!-- 热水器 -->
-  <!-- <ShowModel modelUrl="http://rhkbqy9os.hn-bkt.clouddn.com/%E7%83%AD%E6%B0%B4%E5%99%A8.fbx?e=1663569266&token=OkXHJZMiNUEjV5wqnHmy73EoxxBwOWPqA6co-0Qv:u8tVnX6ndvd7FF5TdUsLxWB6gWU="/> -->
+  <!-- <ShowModel modelUrl="https://syn-yf-design-tool.oss-cn-beijing.aliyuncs.com/save_fbx/AA3Q7C000-卡萨帝-送风-3D微风面板-CMB-015NTB(S)-白.fbx"/> -->
 
   <!-- 洗衣机-->
-  <!-- <ShowModel modelUrl= "http://rhkbqy9os.hn-bkt.clouddn.com/%E6%B5%B7%E5%B0%94-%E6%BB%9A%E7%AD%92-%E5%B9%B2%E8%A1%A3%E6%9C%BA-10kg-HGS100-306.fbx?e=1663744514&token=OkXHJZMiNUEjV5wqnHmy73EoxxBwOWPqA6co-0Qv:rvFOWF3OzGt_Phwa9rmvzhWBDnA="/> -->
+  <!-- <ShowModel modelUrl= "https://syn-yf-design-tool.oss-cn-beijing.aliyuncs.com/save_fbx/AA3RZ0007-模型.fbx"/> -->
   
   <!-- 热水器1-->
-  <!-- <ShowModel modelUrl="http://rhkbqy9os.hn-bkt.clouddn.com/AA35U0000-RFUM100.fbx?e=1663905155&token=OkXHJZMiNUEjV5wqnHmy73EoxxBwOWPqA6co-0Qv:mqeqjA79Xlo5v3MxP8Vl2XSBe3I="/> -->
+  <!-- <ShowModel modelUrl="https://syn-yf-design-tool.oss-cn-beijing.aliyuncs.com/save_fbx/B00VC007T-卡萨帝-四门-电冰箱-777升-BCD-777WLCFDGAD5U1.fbx"/> -->
 
   <!-- 风扇 -->
-  <!-- <ShowModel modelUrl= "http://rhkbqy9os.hn-bkt.clouddn.com/fengshan.fbx?e=1663232853&token=OkXHJZMiNUEjV5wqnHmy73EoxxBwOWPqA6co-0Qv:FKpmgfjP-CiChIPwHCkYocl_Xtk="/> -->
-
-  <!-- 冰箱 -->
-  <!-- <ShowModel modelUrl= "http://rhkbqy9os.hn-bkt.clouddn.com/BC11K3000.fbx?e=1663236284&token=OkXHJZMiNUEjV5wqnHmy73EoxxBwOWPqA6co-0Qv:Maw_XhkDCXzUnENjFsvhPFr84tQ="/> -->
-
-  <!-- <ShowModel modelUrl= "https://mz-reszgb.oss-cn-beijing.aliyuncs.com/lisongming_test/GK0GX300C-空气能热泵-热水器-200L-.fbx"/> -->
+  <!-- <ShowModel modelUrl= "https://syn-yf-design-tool.oss-cn-beijing.aliyuncs.com/save_fbx/B300G003M-BCBD-200HEGW-冷柜.fbx"/> -->
 </template>
